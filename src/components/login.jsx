84b7f575f7d66428b598b87174b993d9159818be
Ange290 <uange209@gmail.com>
2026-01-07 20:02:39 +0200

    Add style to logo

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -5,12 +5,12 @@ function Login() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
    
-      <div className="bg-lime-800 p-8 rounded-xl shadow-lg flex flex-col items-center w-96 border-4 border-red-500">
+      <div className="bg-white p-8 rounded-xl shadow-lg flex flex-col items-center w-96 border-4 ">
       
         <img
           src={ContainerImg}
           alt="Login"
-          className="w-48 h-48 object-contain mb-4"
+          className=""
         />
 
         <p className="text-gray-500 text-center">
