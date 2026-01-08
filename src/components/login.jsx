339ddd349966afff3845860107ae5176dad63b8d
Ange290 <uange209@gmail.com>
2026-01-08 12:20:56 +0200

    Create form

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -13,9 +13,17 @@ function Login() {
           className=""
         />
 
-        <p className="text-gray-500 text-center">
-          Please login to continue
-        </p>
+        <h1 className="text-gray-700 text-center font-outfit font-medium text-2xl ">
+         DrugStock System
+        </h1>
+
+        <form action="" className="font-outfit">
+          <label htmlFor="username">Username</label><br />
+          <input type="text" id="username" name="username" placeholder="Enter your username" /><br /><br />
+          <label htmlFor="password">Password</label><br />
+          <input type="password" id="password" name="password" placeholder="Enter your password" /><br /><br />
+          
+        </form>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html","./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
-         fontFamily: {
-        outfit: ['"Outfit"', ...defaultTheme.fontFamily.sans],
-      }
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'],
+        sans: ['Outfit', 'sans-serif'], // Makes Outfit the default
+      },
     },
   },
   plugins: [],
 }
-
